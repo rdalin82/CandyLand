ad40246fea6d2c3e8d12eb2b5dee2f2bd60b4b5d
rdalin82 <Robert.Dalin@gmail.com>
2015-07-31 22:14:59 -0500

    added step methods and tests

--- lib/step.rb
+++ lib/step.rb
@@ -1,2 +1,10 @@
 class Step
+	attr_reader :colors 
+	def initialize
+		@colors = ['pink', 'purple', 'yellow', 'blue', 'orange', 'green']
+	end 
+
+	def step_color
+		@colors.sample
+	end 
 end 
---
+++ test/step_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require_relative '../lib/step'
+
+describe Step do 
+	before do 
+		@step = Step.new
+	end 
+
+	describe "Should have colors" do 
+		it "Should have 6 colors" do 
+			assert_equal @step.colors.count, 6
+		end 
+
+		it "should have a step color" do 
+			assert_respond_to @step, :step_color
+		end 
+
+		it "step color should return a color" do 
+		    assert @step.colors.include?(@step.step_color)  
+		end 
+
+	end 
+end  
